2013e6458d7e696bfa33aadfda66f421f2193891
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-16 22:47:36 +0800

    feat: add boilerplate component files

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,10 @@
+/**
+ * LoginForm component
+ * @author Kenneth Sumang
+ * @since  2023.05.16
+ */
+export default function LoginForm() {
+  return (
+    <div></div>
+  );
+}
---
+++ components/auth/RegisterForm.tsx
@@ -0,0 +1,10 @@
+/**
+ * RegisterForm component
+ * @author Kenneth Sumang
+ * @since  2023.05.16
+ */
+export default function RegisterForm() {
+  return (
+    <div></div>
+  );
+}
