a9bef4bc3708f5ee6a4e5fa6f2959d2286869973
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-17 14:02:42 +0800

    feat: preliminary login screen design

---
+++ components/auth/AuthContainer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * AuthContainerProps type
+ */
+type AuthContainerProps = {
+  children: React.ReactNode
+};
+
+/**
+ * AuthContainer component
+ * @author Kenneth Sumang
+ * @since  2023.05.17
+ */
+export default function AuthContainer({ children }: AuthContainerProps) {
+  return (
+    <div className="flex flex-row w-full h-full justify-center items-center">
+      {children}
+    </div>
+  );
+}
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -5,6 +5,51 @@
  */
 export default function LoginForm() {
   return (
-    <div></div>
+    <div className="w-full px-5">
+      <div className="mb-6">
+        <h1 className="text-3xl font-bold"> Login </h1>
+        <p> Please sign in to continue. </p>
+      </div>
+      <form>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your email
+          </label>
+
+          <input
+            type="email"
+            id="email"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Email Address"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your password
+          </label>
+
+          <input
+            type="password"
+            id="password"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+
+        <div className="flex flex-row-reverse">
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
