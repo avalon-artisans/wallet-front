684dd33600e6341ed0fda8c94e0f0f2d15ac1746
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-18 20:18:18 +0800

    wip

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,23 @@
 import NextAuth, {AuthOptions} from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 /**
  * Auth options for NextAuth
  */
 export const authOptions: AuthOptions = {
   providers: [
-    // todo
+    CredentialsProvider({
+      async authorize(credentials, req)  {
+        return {
+          email: 'admin@example.com',
+          name: 'Admin'
+        };
+      },
+      credentials: {
+        email: {},
+        password: {},
+      }
+    }),
   ],
   callbacks: {
     session({ session, token, user }) {
