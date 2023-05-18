e86c5973c7e98261d5bf7ef78a437c8d779b7d55
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-18 08:16:05 +0800

    fix: change button text to log in

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -46,7 +46,7 @@ export default function LoginForm() {
 
         <div className="flex flex-row-reverse">
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">
-            Submit
+            Log in
           </button>
         </div>
       </form>
