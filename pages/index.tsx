734509b46415569938d7696ccb0c3c8fb345e3de
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-21 17:36:59 +0800

    fix: styling for large screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-import AuthContainer from "@/components/auth/AuthContainer";
-import LoginForm from "@/components/auth/LoginForm";
+import LoginForm from '@/components/auth/LoginForm';
 
 /**
  * Index page component
@@ -9,22 +8,24 @@ import LoginForm from "@/components/auth/LoginForm";
 export default function Index() {
   return (
     <main className="h-screen w-screen">
-      <div className="flex flex-col h-full w-full">
-        <div className="flex flex-col h-full w-full md:w-3/4 md:self-center">
-          <AuthContainer>
-            <LoginForm />
-          </AuthContainer>
-        </div>
-        <div className="flex flex-col pb-5 items-center w-screen">
-          <div>
-            { "Don't have an account?" }
-            &nbsp;
-            <a
-              href="/register"
-              className="underline text-blue-500 hover:text-blue-800 visited:text-purple-600"
-            >
-              {  "Sign up." }
-            </a>
+      <div className="flex flex-col h-full w-full items-center">
+        <div className="flex flex-col h-full w-full items-center lg:justify-center">
+          <div className="flex flex-col h-full lg:h-auto w-full md:w-3/4 lg:w-1/2">
+            <div className="flex flex-row h-full w-full items-center">
+              <LoginForm />
+            </div>
+          </div>
+          <div className="flex flex-col pb-5 lg:pt-5 items-center w-screen">
+            <div>
+              { "Don't have an account?" }
+              &nbsp;
+              <a
+                href="/register"
+                className="underline text-blue-500 hover:text-blue-800 visited:text-purple-600"
+              >
+                {  "Sign up." }
+              </a>
+            </div>
           </div>
         </div>
       </div>
