286eaff7696e9b6f845009bf39d1b5a22de6b9e2
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-21 17:49:20 +0800

    feat: boilerplate design for register page for mobile.

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -5,6 +5,88 @@
  */
 export default function RegisterForm() {
   return (
-    <div></div>
+    <div className="w-full px-5">
+      <div className="mb-6">
+        <h1 className="text-3xl font-bold"> Register </h1>
+        <p> Enter the following details for your account. </p>
+      </div>
+
+      <form>
+        <div className="mb-6">
+          <label
+            htmlFor="register__name"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your name
+          </label>
+
+          <input
+            type="text"
+            id="register__name"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="John Doe"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="register__email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your email
+          </label>
+
+          <input
+            type="email"
+            id="register__email"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="johndoe@example.com"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="register__password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Your password
+          </label>
+
+          <input
+            type="password"
+            id="register__password"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="register__retype_password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Retype your password
+          </label>
+
+          <input
+            type="password"
+            id="register__retype_password"
+            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+
+        <div className="flex flex-row">
+          <button className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center mr-5">
+            Back
+          </button>
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
---
+++ pages/register.tsx
@@ -0,0 +1,16 @@
+import RegisterForm from '@/components/auth/RegisterForm';
+
+/**
+ * RegisterPage component
+ * @author Kenneth Sumang
+ * @since  2023.05.21
+ */
+export default function Register() {
+  return (
+    <main className="h-screen w-screen">
+      <div className="flex flex-col h-full w-full items-center justify-center">
+        <RegisterForm />
+      </div>
+    </main>
+  );
+}
