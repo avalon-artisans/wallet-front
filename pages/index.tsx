fe99327372986af8aabab9d42775f0de84b35cf8
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-22 11:22:34 +0800

    feat: limit width for xl width screens for login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
     <main className="h-screen w-screen">
       <div className="flex flex-col h-full w-full items-center">
         <div className="flex flex-col h-full w-full items-center lg:justify-center">
-          <div className="flex flex-col h-full lg:h-auto w-full md:w-3/4 lg:w-1/2">
+          <div className="flex flex-col h-full lg:h-auto w-full md:w-3/4 lg:w-1/2 xl:w-1/3">
             <div className="flex flex-row h-full w-full items-center">
               <LoginForm />
             </div>
