4cd6898519921e8733b19a5e98df84e972650b29
Kenneth Sumang <kennethsumang08@gmail.com>
2023-05-30 21:43:07 +0800

    feat(register): add form data validation

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,3 +1,5 @@
+import Validator, {ValidationErrors} from 'validatorjs';
+
 /**
  * UserService class
  * @author Kenneth Sumang
@@ -10,6 +12,62 @@ export default class UserService {
    * @returns {Promise<{ success: boolean, message: string, data?: any }>}
    */
   async processRegistration(data: any): Promise<{ success: boolean, message: string, data?: any }> {
+    const validationResponse = this.validateFormData(data);
+    if (!validationResponse.success) {
+      return {
+        success: false,
+        message: validationResponse.message as string
+      };
+    }
     return { success: true, message: 'Done.' };
+  }
+
+  /**
+   * Checks if the form data is valid.
+   * @param {any} data
+   * @returns {success: boolean, message?: string}
+   */
+  validateFormData(data: any): {success: boolean, message: string|null} {
+    const validationRules = {
+      name: 'required|string',
+      email: 'required|email',
+      password: 'required|string',
+      retypePassword: 'required|same:password'
+    };
+
+    const validationMessages = {
+      'required.name': 'Missing name.',
+      'string.name': 'Invalid name.',
+      'required.email': 'Missing email.',
+      'email.email': 'Invalid email.',
+      'required.password': 'Missing password.',
+      'string.password': 'Invalid password.',
+      'required.retypePassword': 'Missing retype password.',
+      'same.retypePassword': 'Retype password not equal.',
+    };
+
+    const validation = new Validator(data, validationRules, validationMessages);
+    if (validation.passes()) {
+      return {
+        success: true,
+        message: null,
+      };
+    }
+
+    return {
+      success: false,
+      message: this.getFirstErrorMessage(validation.errors.all())
+    }
+  }
+
+  /**
+   * Gets the first error message in the error messages object
+   * @param {ValidationErrors} errors
+   * @returns {string}
+   */
+  getFirstErrorMessage(errors: ValidationErrors): string {
+    const firstKey = Object.keys(errors)[0];
+    const firstEntry = errors[firstKey];
+    return firstEntry[0];
   }
 }
